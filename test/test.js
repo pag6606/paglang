87cfa572aa3a820490e8f700c728ab30686a266e
Paúl Alarcón <pag66.06@gmail.com>
2017-09-22 22:20:13 -0500

    corrigiendo errores de nombre

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
 
 const expect = require('chai').expect
-const paglang = require('..').default
-describe('#paglang', function(){
+const nuevolenguaje = require('..').default
+describe('#nuevolenguaje', function(){
     it('si la palabra termina en ar se le quitan dos utimos caracteres', function(){
-        const translation= paglang("Programar")
+        const translation= nuevolenguaje("Programar")
         expect(translation).to.equal("Program")
     })
     it('si la palabra inicia con z se le añade p al final', function(){
-        const translation= paglang("Zorro")
+        const translation= nuevolenguaje("Zorro")
         expect(translation).to.equal("Zorrope")
     })
     it('si la palabra tiene 10 o mas letras vva guion al medio', function(){
-        const translation= paglang("Zorro")
+        const translation= nuevolenguaje("Zorro")
         expect(translation).to.equal("Zorrope")
     })
     it('si es palindromo  se intercala mayusuculas y minusculas', function(){
-        const translation= paglang("sometemos")
+        const translation= nuevolenguaje("sometemos")
         expect(translation).to.equal("SoMeTeMoS")      
     })
     it('Si la palabra traducida tiene 10 o mas se divide en dos con un guión', function(){
-        const translation= paglang("abecedario")
+        const translation= nuevolenguaje("abecedario")
         expect(translation).to.equal("abece-dario")      
     })
 })
